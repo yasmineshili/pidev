5d73459befccfd7a81364fad7f3557679a451b00
yasmineshili <yasmineshili@esprit.tn>
2023-02-23 01:31:59 +0100

    commit pidev

---
+++ src/Form/FicheType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Fiche;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FicheType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('email')
+            ->add('date')
+            ->add('numtel')
+            ->add('duree')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Fiche::class,
+        ]);
+    }
+}
